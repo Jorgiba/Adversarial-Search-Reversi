dd80acc0dbeaaec665f11fd4fed1249813639d6a
Jorgiba <128039685+Jorgiba@users.noreply.github.com>
2026-03-31 19:38:52 +0200

    Refactor heuristic classes and evaluation functions

--- p2_1322_06_Ibarreta_Rocha.py
+++ p2_1322_06_Ibarreta_Rocha.py
@@ -8,28 +8,147 @@ from tournament import (
     StudentHeuristic,
 )
 
-# das valor a un estado completo que te mandan, no a una casilla en la que colocar ficha
 
 
 def func_glob(n: int, state: TwoPlayerGameState) -> float:
     return n + simple_evaluation_function(state)
 
 
+
+class Solution11(StudentHeuristic):
+  def get_name(self) -> str:
+    return "1pa1SinCamiseta"
+  def evaluation_function(self, state: TwoPlayerGameState) -> float:
+    esqj = [0, 0, 7, 7]
+    esqi = [0, 7, 0, 7]
+    c1=0
+    c2=0
+    if state.end_of_game:
+            if state.is_player_max(state.player1):
+                return 1000
+            return -1000
+          
+    for i in esqi:
+      for j in esqj:
+          pos = state.board.get((i+1, j+1), '_',)
+          if(pos == state.player1.label):
+            c1 = c1+1
+          elif (pos == state.player2.label):
+            c2 = c2+1
+    mov1 = len(state.game._get_valid_moves(state.board,state.player1.label))
+    mov2 = len(state.game._get_valid_moves(state.board,state.player2.label))
+    p1 = state.scores[0]
+    p2 = state.scores[1]
+    if state.is_player_max(state.player1)==False:
+      c1, c2 = c2, c1
+      mov1, mov2 = mov2, mov1
+      p1, p2 = p2, p1
+    result=0
+    if(c1+c2!=0):
+      result=result + (70/100) * ((c1-c2)/(c1+c2)*100)
+    if(mov1+mov2!=0):
+      result=result + (15/100) *((mov1-mov2)/(mov1+mov2)*100)
+    if(p1+p2!=0):
+      result+=result + (15/100) *((p1-p2)/(p1+p2)*100) 
+
+    return result
+
+class CombinedHeuristic(StudentHeuristic):
+    def get_name(self) -> str:
+        return "CombinedRarilla1pa1"
+
+    def evaluation_function(self, state: TwoPlayerGameState) -> float:
+        j1_score = 0
+        j2_score = 0
+        height = state.game.height
+        width = state.game.width
+        centre_y = height // 2
+        centre_x = width // 2
+
+        corner_value = 100
+        critical_position_value = -50
+
+        corners = [(1, 1), (1, width), (height, 1), (height, width)]
+        critical_positions = [
+            (1, 2), (2, 1), (2, 2), (height, 2), (height - 1, 1), (height - 1, 2),
+            (height, width - 1), (height - 1, width), (height - 1, width - 1),
+            (width, 2), (width - 1, 1), (width - 1, 2)
+        ]
+
+        for c in state.board:
+            x, y = c
+            if (x, y) in critical_positions:
+                if state.board[c] is state.player1.label:
+                    j1_score += critical_position_value
+                elif state.board[c] is state.player2.label:
+                    j2_score += critical_position_value
+                continue
+
+            if (x, y) in corners:
+                if state.board[c] is state.player1.label:
+                    j1_score += corner_value
+                elif state.board[c] is state.player2.label:
+                    j2_score += corner_value
+                continue
+            
+            distancia_centro = abs(x - centre_x) + abs(y - centre_y)
+            if 1 < x < width and 1 < y < height:
+                distancia_centro -= 2  
+
+            if state.board[c] is state.player1.label:
+                j1_score += distancia_centro
+            elif state.board[c] is state.player2.label:
+                j2_score += distancia_centro
+
+        esqj = [0, 0, 7, 7]
+        esqi = [0, 7, 0, 7]
+        c1 = 0
+        c2 = 0
+        for i in esqi:
+            for j in esqj:
+                pos = state.board.get((i+1, j+1), '_')
+                if pos == state.player1.label:
+                    c1 += 1
+                elif pos == state.player2.label:
+                    c2 += 1
+        
+        mov_j1 = len(state.game._get_valid_moves(state.board,state.player1.label))
+        mov_j2 = len(state.game._get_valid_moves(state.board,state.player2.label))
+
+        p1 = state.scores[0]
+        p2 = state.scores[1]
+
+        if not state.is_player_max(state.player1):
+            c1, c2 = c2, c1
+            mov_j1, mov_j2 = mov_j2, mov_j1
+            p1, p2 = p2, p1
+
+        result = 0
+        if c1 + c2 != 0:
+            result += (70 / 100) * ((c1 - c2) / (c1 + c2) * 100)
+        if mov_j1 + mov_j2 != 0:
+            result += (15 / 100) * ((mov_j1 - mov_j2) / (mov_j1 + mov_j2) * 100)
+        if p1 + p2 != 0:
+            result += (15 / 100) * ((p1 - p2) / (p1 + p2) * 100)
+
+        j1_score += 7 * mov_j1
+        j2_score += 7 * mov_j2
+
+        if state.is_player_max(state.player1):
+            return j1_score - j2_score + result
+        else:
+            return j2_score - j1_score + result
+
 class Solution2(StudentHeuristic):
     def get_name(self) -> str:
         return "BuenaG"
 
     def evaluation_function(self, state: TwoPlayerGameState) -> float:
-        c1 = 0
-        c2 = 0
-
-        # Comprobamos si el juego ya ha terminado
         if state.end_of_game:
             if state.is_player_max(state.player1):
                 return 1000
             return -1000
 
-        # Mapeados para early game, mid game y late game (tablero 8x8)
         early_game_map = [
             [20, -3, 11, 8, 8, 11, -3, 20],
             [-3, -7, -4, 1, 1, -4, -7, -3],
@@ -63,21 +182,17 @@ class Solution2(StudentHeuristic):
             [100, -15, 25, 20, 20, 25, -15, 100],
         ]
 
-        # Determinamos la fase del juego basándonos en el número de fichas
         num_fichas = sum(1 for pos in state.board if state.board[pos] is not None)
         total_fichas = state.game.height * state.game.width
-        if (
-            num_fichas <= total_fichas * 0.25
-        ):  # Early game: menos del 25% del tablero ocupado
+        if num_fichas <= total_fichas * 0.25:
             board_map = early_game_map
-        elif num_fichas <= total_fichas * 0.75:  # Mid game: entre el 25% y el 75%
+        elif num_fichas <= total_fichas * 0.75:
             board_map = mid_game_map
-        else:  # Late game: más del 75% del tablero ocupado
+        else:
             board_map = late_game_map
 
-        # Calculamos los puntos basados en el mapeado del tablero para cada fase
-        p1 = 0  # Puntos para el jugador 1
-        p2 = 0  # Puntos para el jugador 2
+        p1 = 0
+        p2 = 0 
 
         for i in range(8):
             for j in range(8):
@@ -87,70 +202,23 @@ class Solution2(StudentHeuristic):
                 elif state.board.get(pos) == state.player2.label:
                     p2 += board_map[i][j]
 
-        # Aplicamos los ajustes adicionales (movimientos, esquinas, puntuación)
         mov1 = len(state.game._get_valid_moves(state.board, state.player1.label))
         mov2 = len(state.game._get_valid_moves(state.board, state.player2.label))
         p1_score = state.scores[0]
         p2_score = state.scores[1]
 
-        # Si soy jugador 2, intercambiamos las puntuaciones y movimientos
         if not state.is_player_max(state.player1):
             p1, p2 = p2, p1
             mov1, mov2 = mov2, mov1
             p1_score, p2_score = p2_score, p1_score
 
-        # Resultado final ponderado
         result = 0
-        if (c1 + c2) != 0:
-            result += (70 / 100) * ((c1 - c2) / (c1 + c2) * 100)
+        if (p1 + p2) != 0:
+            result += (70 / 100) * ((p1 - p2) / (p1 + p2) * 100)
         if (mov1 + mov2) != 0:
             result += (15 / 100) * ((mov1 - mov2) / (mov1 + mov2) * 100)
         if (p1_score + p2_score) != 0:
             result += (15 / 100) * ((p1_score - p2_score) / (p1_score + p2_score) * 100)
 
-        # Añadimos el resultado basado en el valor de cada casilla del mapeado
-        result += p1 - p2
 
         return result
-
-
-class Solution11(StudentHeuristic):
-  def get_name(self) -> str:
-    return "1pa1SinCamiseta"
-  def evaluation_function(self, state: TwoPlayerGameState) -> float:
-    esqj = [0, 0, 7, 7]
-    esqi = [0, 7, 0, 7] #esq inf d, inf i, sup d, sup i
-    c1=0
-    c2=0
-    #comprobamos si el juego ya ha terminado
-    if state.end_of_game:
-            if state.is_player_max(state.player1):
-                return 1000
-            return -1000
-          
-    for i in esqi:
-      for j in esqj:
-          pos = state.board.get((i+1, j+1), '_',)
-          if(pos == state.player1.label): #si en esa esquina esta el jugador 1
-            c1 = c1+1 #sumamos 1 a las esquinas de jugador 1
-          elif (pos == state.player2.label): #si en esa esquina esta el jugador 2
-            c2 = c2+1 #sumamos 1 a las esquinas de jugador 2
-          #si no hay nadie en esa esquina aprovechamos para coger los corners closeness
-    mov1 = len(state.game._get_valid_moves(state.board,state.player1.label))
-    mov2 = len(state.game._get_valid_moves(state.board,state.player2.label))
-    p1 = state.scores[0]
-    p2 = state.scores[1]
-    if state.is_player_max(state.player1)==False:
-      c1, c2 = c2, c1
-      mov1, mov2 = mov2, mov1
-      p1, p2 = p2, p1
-    result=0
-    if(c1+c2!=0):
-      result=result + (70/100) * ((c1-c2)/(c1+c2)*100)
-    if(mov1+mov2!=0):
-      result=result + (15/100) *((mov1-mov2)/(mov1+mov2)*100)
-    if(p1+p2!=0):
-      result+=result + (15/100) *((p1-p2)/(p1+p2)*100) 
-
-    return result
-
